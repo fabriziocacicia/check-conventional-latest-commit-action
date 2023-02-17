b06583691ddf8cc71852cd58d92ef61b0238038c
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-02-17 14:35:45 +0100

    fix: attempt to fix detected dubious ownership in repository
    Workaround adding the suggested solution of adding /github/workspace to safe directory in entry point.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
+# Workoround for Error: fatal: detected dubious ownership in repository at '/github/workspace'
+git config --global --add safe.directory /github/workspace
+#
+
 npx commitlint --from HEAD~1 --to HEAD --verbose
